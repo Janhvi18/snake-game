d421a5949a0b167285d200f9b51abdf0ccc4b521
Janhvi Pable <56631887+Janhvi18@users.noreply.github.com>
2021-02-07 22:35:04 +0530

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<conio.h>
+#include<windows.h>
 using namespace std;
 bool gameOver;
 const int width = 20;
@@ -122,6 +123,11 @@ void Logic(){
 	if(x>width || x<0 || y>height || y<0){
 		gameOver=true;
 	}
+	for(int i=0;i<nTail;i++){
+		if(tailX[i]==x && tailY[i]==y){
+			gameOver=true;
+		}
+	}
 	if(x==fruitX && y==fruitY){
 		
 		score+=10;
@@ -136,7 +142,7 @@ int main(){
 		Draw();
 		Input();
 		Logic();
-		//Sleep(10);
+		Sleep(10);
 	
 	}
 }
